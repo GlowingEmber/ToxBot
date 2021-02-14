6217d09730c7024bb54ec4490da25ac20da8c37f
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-02-13 23:59:57 -0800

    basic setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const toxicity = require('@tensorflow-models/toxicity');
+require('@tensorflow/tfjs');
+
+
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+const threshold = 0.9;
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+    meanmessage = message.content.split("?")
+  if (meanmessage[0] === 'toxic') {
+    toxicity.load(threshold).then(model => {
+        model.classify(meanmessage[1]).then(predictions => {
+      
+          for(i=0; i < predictions.length; i++){
+              if(predictions[i].results[0].match === true || predictions[i].results[0].match === null){
+                  message.channel.send(predictions[i].label)
+              }
+          }
+        });
+      });
+  }
+});
+
+client.login('ODEwNDEyNTg3NTk3NjkyOTM4.YCjRiQ.z_TPB_ZoqsgaOIbw5th8zGt2gk8');
